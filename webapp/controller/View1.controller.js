d50caaf22a5833f5522fb9a49373a338dfccef72
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-05 23:54:36 +0530

    Updated controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,6 +6,11 @@ sap.ui.define([
 	return Controller.extend("GithubSAP-UI5-Fiori-16_Routing.controller.View1", {
 		
 		onInit : function(){
+			
+			/*var oInfo = new sap.ui.model.json.JSONModel();
+			oInfo.loadData("model/student.json");
+			this.getView().setModel(oInfo);*/       //no need to write this line for model if you define your model in manifes.json
+			
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getRoute("View1");
 		},
